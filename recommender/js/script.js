6bc38bb37c8d6c52637538cb33a2637254d13bf3
Nayan Seth <nayantronix@gmail.com>
2016-11-06 16:02:40 -0700

    JSON
    Load JSON in JS

--- recommender/js/script.js
+++ recommender/js/script.js
@@ -1,10 +1,54 @@
+numViews = 0, numVotes = 0, numAnswers = 0;
+
+var request;
+var data;
+
+function loadData(callback) {
+
+  if(window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+  } else {
+    request = new ActiveObject("Microsoft.XMLHTTP");
+  }
+
+  request.open("GET","data/data.json");
+
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
 function OnChange(id) {
 
   if(id=="votes-text") {
+    numVotes = document.getElementById("votes").value;
     document.getElementById(id).innerHTML = "Votes: " + document.getElementById("votes").value;
   } else if (id=="views-text") {
+    numViews = document.getElementById("views").value;
     document.getElementById(id).innerHTML = "Views: " + document.getElementById("views").value;
   } else {
+    numAnswers = document.getElementById("answers").value;
     document.getElementById(id).innerHTML = "Answers: " + document.getElementById("answers").value;
   }
+
+  recommendPosts(numVotes,numViews,numAnswers);
+
+}
+
+function recommendPosts(votes,views,answers) {
+
+  console.log(votes);
+  console.log(views);
+  console.log(answers);
+}
+
+function init() {
+ loadData(function(response) {
+  // Parse JSON string into object
+    data = JSON.parse(response);
+ });
 }
