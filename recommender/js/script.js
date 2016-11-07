79c99740fb1857a3e1088a0dac28a680f68f194a
Nayan Seth <nayantronix@gmail.com>
2016-11-06 17:13:40 -0700

    Stopword Removal
    Removed stop words from question!

--- recommender/js/script.js
+++ recommender/js/script.js
@@ -1,14 +1,10 @@
 numViews = 0, numVotes = 0, numAnswers = 0;
 
+var stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"];
+
 var request;
+
 var data;
-
-function extractQuestion(){
-  var question=document.getElementById('question').value;
-  question=question.split(' ');
-  console.log(question);
-
-}
 
 function loadData(callback) {
 
@@ -27,6 +23,27 @@ function loadData(callback) {
     }
   };
   request.send(null);
+}
+
+
+function extractQuestion(){
+  var question=document.getElementById('question').value;
+  question=question.split(' ');
+
+  for(var i =0;i<question.length;i++) {
+
+    for(var j =0;j<stopwords.length;j++) {
+      if(question[i]==stopwords[j]) {
+        question.splice(i,1);
+        i--;
+        break;
+      }
+    }
+
+  }
+
+  return question;
+
 }
 
 function OnChange(id) {
@@ -48,9 +65,6 @@ function OnChange(id) {
 
 function recommendPosts(votes,views,answers) {
 
-  console.log(votes);
-  console.log(views);
-  console.log(answers);
 }
 
 function init() {
